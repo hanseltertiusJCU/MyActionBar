e7dfabe9dd41ff0fb8abd6dd490b28077dee7c71
Hansel Tertius <hansel.tertius@my.jcu.edu.au>
2019-02-18 14:22:22 +0700

    Implement Action Bar menu

--- app/src/main/java/com/example/myactionbar/MainActivity.java
+++ app/src/main/java/com/example/myactionbar/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.myactionbar;
 
+import android.content.Intent;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.menu1: // Set fragment ke activity
+                MenuFragment menuFragment = new MenuFragment();
+                FragmentManager mFragmentManager = getSupportFragmentManager();
+                FragmentTransaction mFragmentTransaction = mFragmentManager.beginTransaction();
+                mFragmentTransaction.replace(R.id.fragment_container,menuFragment);
+                mFragmentTransaction.addToBackStack(null);
+                mFragmentTransaction.commit();
+                return true;
+            case R.id.menu2: // Pindah activity baru
+                Intent i = new Intent(this,MenuActivity.class);
+                startActivity(i);
+                return true;
+            default:
+                return true;
+        }
     }
 }
